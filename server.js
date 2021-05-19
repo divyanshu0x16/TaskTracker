5ef14f6d3a1f0a47e420cc37408a7c72ad7e6fff
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-05-19 22:25:02 +0530

    final server.js

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8000;
-const express = require('express');
-const path = require('path');
 
 server.use(middlewares);
 server.use(router);
