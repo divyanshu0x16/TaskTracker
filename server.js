111492a767c8e5a96f5e3eaa372b6b99dac2b860
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-05-20 00:20:47 +0530

    final server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const app = jsonServer.create();
+const path = require('path');
+const express = require('express');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8000;
+const router = jsonServer.router('db.json');
+const port = process.env.PORT || 3001;
 
-server.use(middlewares);
-server.use(router);
+app.use('/db', middlewares, router);
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 server.listen(port);
